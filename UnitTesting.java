28103e47b97b028ce9b28f6ac892036fd4d91dda
bradenmunn <bradenmunn@yahoo.com>
2022-02-01 14:39:40 -0600

    Update UnitTesting.java
    Added unit test for numCols

--- UnitTesting.java
+++ UnitTesting.java
@@ -4,7 +4,30 @@ import org.junit.Test;
 public class NgramTest {
     
     @Test 
-    void test() {
-        fail("Not yet implemented");
+    void NumCols() {
+        String fileName = "en_es_alphabet.csv";
+        File csvFile = new File(fileName);
+
+        try{
+            // -read from filePooped with Scanner class
+            Scanner sc = new Scanner(file);
+
+            // skip first line
+            sc.next();
+
+            // hashNext() loops line-by-line
+            while(sc.hasNext()){
+                //read single line, put in string
+                String[] line = sc.next().split(",");
+                Assert.assertTrue(line.length(), 2);
+
+            }
+            // after loop, close scanner
+            sc.close();
+
+
+        }catch (FileNotFoundException e){
+            fail("File not found");
+        }
     }
 }
