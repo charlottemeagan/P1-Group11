d7acba5c355a774baef3d3496a33386be35f0f87
zn375582 <47228143+zn375582@users.noreply.github.com>
2022-02-01 14:35:43 -0600

    Update amazon vm commands

--- p1.sh
+++ p1.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
+#!/bin/sh                                                                                               
 echo "Installing openJDK 11..."
-#sudo apt-get install openjdk-11-jdk
+#sudo apt-get install openjdk-11-jdk                                                                    
 
-#Ask for the path of the key they downloaded
+#Ask for the path of the key they downloaded                                                            
 echo "Enter the full path of the key-pair you downloaded: (ex: /Users/username/Downloads)"
 read key_path
 
-#Ask for where they want to download the csv from the vm to
+#Ask for where they want to download the csv from the vm to                                             
 echo "Enter the full path of where you want to download the csv: (ex: /Users/username/Downloads)"
 read csv_path
-echo "If you are prompted, type 'yes'" 
+echo "If you are prompted, type 'yes'"
 
-#Set key permissions (to avoid errors)
-chmod 600 $key_path/Team11Capstone.pem
+#Set key permissions (to avoid errors)                                                                  
+chmod 400 $key_path/Team11Capstone.pem
 
-#Download csv from vm instance
+#Download csv from vm instance                                                                          
 echo "Downloading en_es_alphabet.csv ..."
-scp -i $key_path/Team11Capstone.pem ec2-user@54.147.48.55:/home/ec2-user/downloads/en_es_alphabet.csv $csv_path/en_es_alphabet.csv
+scp -i $key_path/Team11Capstone.pem ec2-user@ec2-100-27-27-235.compute-1.amazonaws.com:/home/ec2-user/downloads/en_es_alphabet.csv $csv_path/en_es_alphabet.csv
 
-#Open the vm?
+#Open the vm?                                                                                           
 echo "Opening the vm... "
 echo "Type 'exit' to exit the vm..."
-ssh -i $key_path/Team11Capstone.pem ec2-user@54.147.48.55
+ssh -i $key_path/Team11Capstone.pem ec2-user@ec2-100-27-27-235.compute-1.amazonaws.com
 
