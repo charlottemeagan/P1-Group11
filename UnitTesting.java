a7feb8189bc460c4e1ae535b95de4292800d11a2
bradenmunn <bradenmunn@yahoo.com>
2022-02-01 18:35:45 -0600

    Update UnitTesting.java
    Pushing last version from VSCode

--- UnitTesting.java
+++ UnitTesting.java
@@ -1,37 +1,28 @@
-import static org.junit.Assert.fail;
-
+import static org.junit.Assert.*;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import org.junit.Test;
+
 
 public class UnitTesting {
     
     @Test 
     void NumCols() {
+        // Get csv file
         String fileName = "en_es_alphabet.csv";
         File csvFile = new File(fileName);
+        Scanner sc = new Scanner(csvFile);
 
-        try{
-            // -read from filePooped with Scanner class
-            Scanner sc = new Scanner(csvFile);
+        // skip first line
+        sc.next();
 
-            // skip first line
-            sc.next();
-
-            // hashNext() loops line-by-line
-            while(sc.hasNext()){
-                //read single line, put in string
-                String[] line = sc.next().split(",");
-                assertTrue(line.length, 2);
-            }
-            // after loop, close scanner
-            sc.close();
-
-
-        }catch (FileNotFoundException e){
-            fail("File not found");
+        // verify each line splits into 2 parts (ie. 2 col inputs for each row)
+        while(sc.hasNext()){
+            //read single line, put in string
+            String[] line = sc.next().split(",");
+            Assert.assertTrue(line.length, 2);
         }
+        // after loop, close scanner
+        sc.close();
     }
 }
